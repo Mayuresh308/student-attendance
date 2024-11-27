046821a010ec714064a024a58d3c9dc10ca8cf83
Mayuresh308 <mayureshmayuresh56@gmail.com>
2024-11-27 17:04:15 +0530

    Updated files

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -77,7 +77,7 @@ const LoginPage = ({ role }) => {
         const password = "zxc"
 
         if (role === "Admin") {
-            const email = "yogendra@12"
+            const email = "mayuresh@30"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
